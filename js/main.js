bec848d673037d999ce820704ccd1316b6ca1fb2
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-09-18 23:22:06 -0400

    Update file structure

---
+++ js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+$(document).ready(() => {
+
+const searchResults = $('#search-results-template').html();
+const moreInfo = $('#more-info-modal').html();
+
+const searchTemplate = Handlebars.compile(searchResults);
+const modalTemplate = Handlebars.compile(moreInfo);
+
+const dummyData = {
+  Search: [
+    {
+      State: 'MISSOURI',
+      Type: 'HUNTING',
+      County: 'Knox County',
+      Animal: 'Deer',
+      Season: 'Archery Season',
+      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+    },
+    {
+      State: 'MISSOURI',
+      Type: 'HUNTING',
+      County: 'Moniteau County',
+      Animal: 'Deer',
+      Season: 'Archery Season',
+      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16' },
+    {
+      State: 'MISSOURI',
+      Type: 'HUNTING',
+      County: 'Miller County',
+      Animal: 'Deer',
+      Season: 'Archery Season',
+      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+    },
+    { State: 'MISSOURI',
+    Type: 'HUNTING',
+    County: 'Cole County',
+    Animal: 'Deer',
+    Season: 'Archery Season',
+    Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+  },
+    {
+      State: 'MISSOURI',
+      Type: 'HUNTING',
+      County: 'Adair County',
+      Animal: 'Deer',
+      Season: 'Archery Season',
+      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+    },
+    {
+      State: 'MISSOURI',
+      Type: 'HUNTING',
+      County: 'York County',
+      Animal: 'Deer',
+      Season: 'Archery Season',
+      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+    }
+  ]
+};
+
+
+const context = modalTemplate(dummyData);
+$('#info-modal').html(context);
+$('#info-modal').css('visibility', 'visible');
+
+
+// closing brackets
+});
