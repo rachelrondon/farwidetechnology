ac1f750a467d0ba0299fa463bf5ea7659243f6c2
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-09-19 23:27:55 -0400

    update season click-through

--- js/main.js
+++ js/main.js
@@ -12,77 +12,53 @@ const dummyData = {
       State: 'MISSOURI',
       Type: 'HUNTING',
       County: 'Knox County',
-      Animal: 'Deer',
-      Season: 'Archery Season',
-      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+      Animal: 'Deer'
+      // Season: ['Archery Season', 'Early Youth Portion', 'November Portion'],
+      // Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
     },
     {
       State: 'MISSOURI',
       Type: 'HUNTING',
       County: 'Moniteau County',
-      Animal: 'Deer',
-      Season: 'Archery Season',
-      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16' },
+      Animal: 'Deer'
+      // Season: ['Archery Season', 'Early Youth Portion', 'November Portion'],
+      // Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16' },
+    },
     {
       State: 'MISSOURI',
       Type: 'HUNTING',
       County: 'Miller County',
-      Animal: 'Deer',
-      Season: 'Archery Season',
-      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+      Animal: 'Deer'
+      // Season: ['Archery Season', 'Early Youth Portion', 'November Portion'],
+      // Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
     },
-    { State: 'MISSOURI',
+    {
+    State: 'MISSOURI',
     Type: 'HUNTING',
     County: 'Cole County',
-    Animal: 'Deer',
-    Season: 'Archery Season',
-    Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+    Animal: 'Deer'
+    // Season: ['Archery Season', 'Early Youth Portion', 'November Portion'],
+    // Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
   },
     {
       State: 'MISSOURI',
       Type: 'HUNTING',
       County: 'Adair County',
-      Animal: 'Deer',
-      Season: 'Archery Season',
-      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+      Animal: 'Deer'
+      // Season: ['Archery Season', 'Early Youth Portion', 'November Portion'],
+      // Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
     },
     {
       State: 'MISSOURI',
       Type: 'HUNTING',
       County: 'York County',
-      Animal: 'Deer',
-      Season: 'Archery Season',
-      Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
+      Animal: 'Deer'
+      // Season: ['Archery Season', 'Early Youth Portion', 'November Portion'],
+      // Dates: 'Sep 01 - Sep 14 | Oct 02 - Nov 16'
     }
-  ]
+  ],
+
 };
-
-const dropdownData = {
-  Dropdown: [
-    {
-      Title: 'Area Size (Ascending)'
-    },
-    {
-      Title: 'Area Size (Descending)'
-    },
-    {
-      Title: 'Area Name (A to Z)'
-    },
-    {
-      Title: 'Season (Soonest First)'
-    }
-  ]
-};
-
-// const moreInfo = $('#more-info-modal').html();
-// const modalTemplate = Handlebars.compile(moreInfo);
-
-// const dropdownInfo = $('#dropdown-info-modal').html();
-// const modalTemplate = Handlebars.compile(dropdownInfo);
-
-const dropdownContext = modalTemplate(dropdownData);
-$('dropdown-modal').html(dropdownContext);
-$('dropdown-modal').css('visibility', 'visible');
 
 
 const context = modalTemplate(dummyData);
@@ -104,6 +80,42 @@ $('.dropdown-toggle').click(function() {
 });
 
 
+let seasonsArray = ['Archery Season', 'Early Youth Portion', 'November Portion'];
+// let index = 1;
+// let seasonsH1 = $('.archery-season');
+
+$('.archery-season').html(seasonsArray[0]);
+// $('.archery-season').css('visibility', 'visible');
+// $('.archery-season').css('font-color', '#0DFF8B');
+
+let count = 0;
+
+
+$(".arrow-forward-material").on('click', function(){
+    count += 1;
+    // console.log(count);
+    let index = seasonsArray[0]
+
+    for (var i = 0; i < count; i++) {
+      for (let j = 0; j < seasonsArray.length; j++) {
+      $('.archery-season').html(seasonsArray[i]);
+      }
+    }
+});
+
+
+$(".arrow-back-material").on('click', function(){
+  // let reverseArray = seasonsArray.reverse()
+    count -= 1;
+    // console.log(count);
+    let index = seasonsArray[0]
+
+    for (var i = 0; i < count; i++) {
+      for (let j = 0; j < seasonsArray.length; j++) {
+      $('.archery-season').html(seasonsArray[i]);
+      }
+    }
+});
 
 
 
